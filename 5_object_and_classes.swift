ce46fff12b66e6c5e7745c6cad39b259f7641825
mono-glyceride <glyceride.mono@gmail.com>
2024-02-18 22:37:12 +0900

    feat: 新しい値を設定する前後で特定のコードを実行（willSet didSet）

--- 5_object_and_classes.swift
+++ 5_object_and_classes.swift
@@ -102,3 +102,25 @@ var triangle = EquilateralTriangle(sideLength: 3.1, name: "a triangle")
 print(triangle.perimeter)
 triangle.perimeter = 9.9
 print(triangle.sideLength)
+
+class TriangleAndSquare {
+  var triangle: EquilateralTriangle {
+    willSet {
+      square.sideLength = newValue.sideLength
+    }
+  }
+  var square: Square {
+    willSet {
+      triangle.sideLength = newValue.sideLength
+    }
+  }
+  init(size: Double, name: String) {
+    square = Square(sideLength: size, name: name)
+    triangle = EquilateralTriangle(sideLength: size, name: name)
+  }
+}
+var triangleAndSquare = TriangleAndSquare(size: 10, name: "another test shape")
+print(triangleAndSquare.square.sideLength)
+print(triangleAndSquare.triangle.sideLength)
+triangleAndSquare.square = Square(sideLength: 50, name: "larger square")
+print(triangleAndSquare.triangle.sideLength)
