0f9e76b14a5fb0efbec35536d4423e305169bfa3
mono-glyceride <glyceride.mono@gmail.com>
2024-02-18 14:48:31 +0900

    feat: 型の変換

--- 2_var_and_let.swift
+++ 2_var_and_let.swift
@@ -10,3 +10,9 @@ let explicitDouble: Double = 70
 // Experiment 値が4で、floatを明示的に指定した定数を作成してみましょう
 let four: Float = 4
 print(four)
+
+// 値は暗黙的に他の型に変換されない
+let label = "The width is "
+let width = 94
+let widthLabel = label + String(width)
+print(widthLabel)
