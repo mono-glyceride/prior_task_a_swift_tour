bc6c6fa8ecd1c2735c73e99573f14c6acbd308ac
mono-glyceride <glyceride.mono@gmail.com>
2024-02-18 17:23:49 +0900

    feat: Experience 関数の引数の変更

--- 4_functions_and_closers.swift
+++ 4_functions_and_closers.swift
@@ -1,4 +1,4 @@
-func greet(person: String, day: String) -> String {
-  return "Hello \(person), today is \(day)"
+func greet(person: String, lunch: String) -> String {
+  return "Hello \(person), today's special lunch is \(lunch)."
 }
-print(greet(person: "Bob", day: "Tuesday"))
+print(greet(person: "Bob", lunch: "sandwich"))
