e54265797b03fa8e15b91941018f1ea178cd5487
mono-glyceride <glyceride.mono@gmail.com>
2024-02-18 21:54:05 +0900

    feat: クラスのイニシャライザ

--- 5_object_and_classes.swift
+++ 5_object_and_classes.swift
@@ -15,3 +15,15 @@ var shape = Shape()
 shape.numberOfSides = 7
 var shapeDescription = shape.simpleDescription()
 print(shapeDescription)
+
+class NameShape {
+  var numberOfSides: Int = 0
+  var name: String
+  init(name: String) {
+    self.name = name
+  }
+
+  func simpleDescription() -> String {
+    return "A shape with \(numberOfSides) sides"
+  }
+}
