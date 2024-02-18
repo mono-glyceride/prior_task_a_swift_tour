513154806fb2b823087a6dd585961bd629ea2c4e
mono-glyceride <glyceride.mono@gmail.com>
2024-02-18 17:31:24 +0900

    feat: 独自の引数ラベルを関数に与える方法

--- 4_functions_and_closers.swift
+++ 4_functions_and_closers.swift
@@ -2,3 +2,22 @@ func greet(person: String, lunch: String) -> String {
   return "Hello \(person), today's special lunch is \(lunch)."
 }
 print(greet(person: "Bob", lunch: "sandwich"))
+
+func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
+  var min = scores[0]
+  var max = scores[0]
+  var sum = 0
+
+  for score in scores {
+    if score > max {
+      max = score
+    } else if score < min {
+      min = score
+    }
+    sum += score
+  }
+  return (min, max, sum)
+}
+let statistics = calculateStatistics(scores: [5, 3, 100, 3, 9])
+print(statistics.sum)
+print(statistics.2)
