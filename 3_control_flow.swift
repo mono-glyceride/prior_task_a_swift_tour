45b00860ae3e6ed55f926f9dd100f2403bb408a8
mono-glyceride <glyceride.mono@gmail.com>
2024-02-18 15:39:38 +0900

    feat: nilの可能性を含む値のif文での扱い

--- 3_control_flow.swift
+++ 3_control_flow.swift
@@ -15,3 +15,13 @@ let scoreDecoration = if teamScore > 10 {
   ""
 }
 print("Score:", teamScore, scoreDecoration)
+
+var optionalString: String? = "Hello"
+print(optionalString == nil)
+
+var optionalName: String? = "John Appleseed"
+var greeting = "Hello!"
+if let name = optionalName {
+  greeting = "Hello, \(name)"
+}
+print(greeting)
