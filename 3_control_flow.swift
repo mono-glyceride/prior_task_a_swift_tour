ddec52c2e958d4c121713bc176736c1b77e663e6
mono-glyceride <glyceride.mono@gmail.com>
2024-02-18 16:10:26 +0900

    feat: whileを用いた反復処理

--- 3_control_flow.swift
+++ 3_control_flow.swift
@@ -66,3 +66,15 @@ for (kindOfNumber, numbers) in interestingNumbers {
 }
 
 print(kindOfLargest, largest)
+
+var n = 2
+while n < 100 {
+  n *= 2
+}
+print(n)
+
+var m = 2
+repeat {
+  m *= 2
+} while m < 100
+print(m)
