cb3b31bf8f0bb623747091aa9eb4727c2755eba7
mono-glyceride <glyceride.mono@gmail.com>
2024-02-18 21:18:53 +0900

    feat: Experiment 定数プロパティと引数を持つメソッドをクラスに追加

--- 5_object_and_classes.swift
+++ 5_object_and_classes.swift
@@ -1,6 +1,12 @@
 class Shape {
   var numberOfSides = 0
+  let width = 10
   func simpleDescription() -> String {
     return "A shape with \(numberOfSides) sides."
   }
+  func isWidthLonger(length: Int) -> Bool {
+    return length < width
+  }
 }
+
+//Experiment letを使用して定数プロパティを追加してください。また。引数を受け取る別のメソッドも追加してみましょう。
