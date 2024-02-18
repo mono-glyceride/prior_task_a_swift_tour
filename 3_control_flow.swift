06f0fd1084c0ba84f73c2fe232deeccbcd0a749d
mono-glyceride <glyceride.mono@gmail.com>
2024-02-18 15:45:56 +0900

    feat: Experience

--- 3_control_flow.swift
+++ 3_control_flow.swift
@@ -19,9 +19,13 @@ print("Score:", teamScore, scoreDecoration)
 var optionalString: String? = "Hello"
 print(optionalString == nil)
 
-var optionalName: String? = "John Appleseed"
+var optionalName: String? = nil
 var greeting = "Hello!"
 if let name = optionalName {
   greeting = "Hello, \(name)"
+} else {
+  greeting = "Hello, guest user"
 }
 print(greeting)
+
+// Experiment optionalNameをnilに変えてみましょう。どんな挨拶が出力されるでしょうか？　optionalNameがnilの場合に別の挨拶が出力されるようにelseを追加してみましょう。
