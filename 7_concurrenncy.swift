64fa59a40087d3fc1a7716649b57217aa4e86f9b
mono-glyceride <glyceride.mono@gmail.com>
2024-02-19 21:13:47 +0900

    feat: 非同期関数の呼び出し

--- 7_concurrenncy.swift
+++ 7_concurrenncy.swift
@@ -1,6 +1,14 @@
-func fetchUseID(from server: String) async -> Int {
+func fetchUserID(from server: String) async -> Int {
   if server == "primary" {
     return 97
   }
   return 501
+}
+
+func fetchUsername(from server: String) async -> String {
+  let userID = await fetchUserID(from: server)
+  if userID == 501 {
+    return "John appleseed"
+  }
+  return "Guest"
 }
