dbffd69e4b7e8b69ad2b450e5f997f9461914a4f
mono-glyceride <glyceride.mono@gmail.com>
2024-02-19 18:26:28 +0900

    feat: 構造体

--- 6_enumerations_and_structures.swift
+++ 6_enumerations_and_structures.swift
@@ -85,3 +85,14 @@ switch success {
 }
 
 //Experiment ServeResponseに3つ目のケースを追加して、switch文を変更してください。
+
+struct Card {
+  var rank: Rank
+  var suit: Suit
+  func simpleDescription() -> String {
+    return "The \(rank.simpleDescription()) of \(suit.simpleDescription()))"
+  }
+}
+let threeOfSpades = Card(rank: .three, suit: .spades)
+let threeOfSpadesDescription = threeOfSpades.simpleDescription()
+print(threeOfSpades)
