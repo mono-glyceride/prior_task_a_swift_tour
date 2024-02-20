299ec1a0b47432a88916a5790d6898b7da821678
mono-glyceride <glyceride.mono@gmail.com>
2024-02-20 10:12:00 +0900

    feat: タスクグループを用いて同時並行コードを構造化

--- 7_concurrenncy.swift
+++ 7_concurrenncy.swift
@@ -24,3 +24,16 @@ await connecUser(to: "async test")
 Task {
   await connecUser(to: "primary")
 }
+
+let userIDs = await withTaskGroup(of: Int.self) { group in
+  for server in ["primary", "secondary", "development"] {
+    group.addTask {
+      return await fetchUserID(from: server)
+    }
+  }
+  var results: [Int] = []
+  for await result in group {
+    result.append(result)
+  }
+  return results
+}
