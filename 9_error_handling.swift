9dfc859628f3e5f2c3f8e4f805fa55b9b673b594
mono-glyceride <glyceride.mono@gmail.com>
2024-02-20 12:07:34 +0900

    feat: throwを用いたエラーのスロー

--- 9_error_handling.swift
+++ 9_error_handling.swift
@@ -3,3 +3,10 @@ enum PrinterError: Error {
   case noToner
   case onFire
 }
+
+func send(job: Int, toPrinter printerName: String) throws -> String {
+  if printerName == "Never Has Toner" {
+    throw PrinterError.noToner
+  }
+  return "Job sent"
+}
