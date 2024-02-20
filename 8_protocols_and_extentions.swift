7742e007a07dab82b57de4204567146f65fff0d3
mono-glyceride <glyceride.mono@gmail.com>
2024-02-20 10:24:48 +0900

    feat: プロトコルに準拠したクラス

--- 8_protocols_and_extentions.swift
+++ 8_protocols_and_extentions.swift
@@ -2,3 +2,14 @@ protocol ExampleProtocol {
   var simpleDescription: String { get }
   mutating func adjust()
 }
+
+class SimpleClass: ExampleProtocol {
+  var simpleDescription: String = "A very simple class"
+  var anotherProperty: Int = 69105
+  func adjust() {
+    simpleDescription += " Now 100% adjusted"
+  }
+}
+var a = SimpleClass()
+let aDescription = a.simpleDescription
+print(aDescription)
