408910caa05e0d0a7be75f1e2ebf4be8d780b679
mono-glyceride <glyceride.mono@gmail.com>
2024-02-20 12:55:09 +0900

    feat: whereで要件の一覧を指定

--- 10_generics.swift
+++ 10_generics.swift
@@ -13,3 +13,17 @@ enum OptionalValue<Wrapped> {
 }
 var possibleInteger: OptionalValue<Int> = .none
 possibleInteger = .some(100)
+
+func anyCommonElements<T: Sequence, U: Sequence>(_ lhs:T, _ rhs: U) -> Bool
+where T.Element: Equatable, T.Element == U.Element
+{
+  for lhsItem in lhs {
+    for rhsItem in rhs {
+      if lhsItem == rhsItem {
+        return true
+      }
+    }
+  }
+  return false
+}
+print(anyCommonElements([1, 2, 3], [3]))
