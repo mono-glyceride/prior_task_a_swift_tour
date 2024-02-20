494ec8547ef88cda4a5f3c288aaf701584d4a82c
mono-glyceride <glyceride.mono@gmail.com>
2024-02-20 10:52:39 +0900

    feat: Experiment プロトコルに新しい要件を追加

--- 8_protocols_and_extentions.swift
+++ 8_protocols_and_extentions.swift
@@ -1,11 +1,13 @@
 protocol ExampleProtocol {
   var simpleDescription: String { get }
+  var number: Int { get }
   mutating func adjust()
 }
 
 class SimpleClass: ExampleProtocol {
   var simpleDescription: String = "A very simple class"
   var anotherProperty: Int = 69105
+  var number: Int = 1
   func adjust() {
     simpleDescription += " Now 100% adjusted"
   }
@@ -13,9 +15,11 @@ class SimpleClass: ExampleProtocol {
 var a = SimpleClass()
 let aDescription = a.simpleDescription
 print(aDescription)
+print(a.number)
 
 struct SimpleStructure: ExampleProtocol {
   var simpleDescription: String = "A simple structure"
+  var number: Int = 2
   mutating func adjust() {
   simpleDescription += " (adjusted)"
   }
@@ -24,3 +28,10 @@ var b = SimpleStructure()
 b.adjust()
 let bDescription = b.simpleDescription
 print(bDescription)
+print(b.number)
+
+/*
+Experiment
+ExperimentProtocolrankにもう一つの要件を追加してみましょう。SimpleClassとSimpleStructureには
+どういう変更が必要になるでしょうか
+*/
